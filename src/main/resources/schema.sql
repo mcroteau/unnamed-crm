7aeed5058aaa6666eae2f79ef39247e0b32a145f
plsa-r <mike@ae0n.net>
2023-03-03 06:46:56 -0900

    .

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,12 +9,7 @@ create table users (
     email character varying(254) NOT NULL,
     passwd character varying(155) NOT NULL,
     image text,
-    onboarded boolean default false,
-    stripe_customer_id text,
-    has_bank boolean default false,
-    has_cell boolean default false,
-    time_created bigint default 0,
-    sponsor boolean default false
+    time_created bigint default 0
 );
 
 create table roles (
@@ -30,4 +25,26 @@ create table user_permissions(
 create table user_roles(
     role_id bigint NOT NULL REFERENCES roles(id),
     user_id bigint NOT NULL REFERENCES users(id)
+);
+
+create table company (
+    id bigint PRIMARY KEY AUTO_INCREMENT,
+    uid character varying(154),
+    guid character varying(154),
+    name character varying(253) NOT NULL,
+    phones text,
+    emails text,
+    image clob,
+    stripe_customer_id text
+);
+
+create table client (
+    id bigint PRIMARY KEY AUTO_INCREMENT,
+    uid character varying(254),
+    guid character varying(254),
+    name character varying(155) NOT NULL,
+    phone character varying(40),
+    email character varying(254) NOT NULL,
+    passwd character varying(155) NOT NULL,
+    time_created bigint default 0
 );
